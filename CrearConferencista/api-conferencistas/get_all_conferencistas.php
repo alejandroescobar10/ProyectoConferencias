e0f4ca215a4092f6f104811198db0c524302177b
alejandroescobar10 <160554099+alejandroescobar10@users.noreply.github.com>
2024-11-11 21:25:08 -0500

    se termina la vista de conferencista

--- CrearConferencista/api-conferencistas/get_all_conferencistas.php
+++ CrearConferencista/api-conferencistas/get_all_conferencistas.php
@@ -1,6 +1,6 @@
 <?php
-require_once '../includes/Database.class.php';
-require_once '../includes/Conferencista.class.php';
+require_once '../../conferencias/models/Database.php';
+require_once '../models/Conferencista.class.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $conferencistas = Conferencista::get_all_conferencistas();
--- login/models/User.php
+++ login/models/User.php
@@ -6,6 +6,7 @@ class User
 
     public function __construct($db)
     {
+
         $this->db = $db;
     }
 
